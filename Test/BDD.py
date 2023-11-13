28c21a6ea2f221c7fbc60c805559f9c33336cd8e
Ivan Mustiere <ivanmustiere.pro@gmail.com>
2023-11-13 13:23:27 +0100

    Test de la bdd

--- Test/BDD.py
+++ Test/BDD.py
@@ -1,36 +1,42 @@
-import mysql.connector
-from mysql.connector import Error
+import psycopg2
+from psycopg2 import OperationalError
 
 
-def creer_connexion(host_name, user_name, user_password, db_name):
-    """Crée une connexion au serveur MySQL.
-    :param host_name: nom de l'hôte ou adresse IP du serveur MySQL.
-    :param user_name: nom d'utilisateur MySQL.
-    :param user_password: mot de passe MySQL.
+def creer_connexion(host_name, port, user_name, user_password, db_name):
+    """Crée une connexion au serveur PostgreSQL.
+    :param host_name: nom de l'hôte ou adresse IP du serveur PostgreSQL.
+    :param port: port du serveur PostgreSQL.
+    :param user_name: nom d'utilisateur PostgreSQL.
+    :param user_password: mot de passe PostgreSQL.
     :param db_name: nom de la base de données à laquelle se connecter.
     :return: objet de connexion ou None.
     """
     connexion = None
     try:
-        connexion = mysql.connector.connect(
-            host=host_name, user=user_name, passwd=user_password, database=db_name
+        connexion = psycopg2.connect(
+            host=host_name,
+            port=port,
+            user=user_name,
+            password=user_password,
+            dbname=db_name,
         )
-        print("Connexion à la base de données MySQL réussie")
-    except Error as e:
+        print("Connexion à la base de données PostgreSQL réussie")
+    except OperationalError as e:
         print(f"L'erreur '{e}' est survenue")
 
     return connexion
 
 
 # Remplacez les valeurs suivantes par vos informations de connexion
-host = "127.0.0.1"
-user = "root"
-password = ""
-database = "mediadb"
+host = "bd.ilovebeemo.com"
+port = 35475
+user = "Test"
+password = "Ivan44119"
+database = "LeagueBet"
 
 # Créer une connexion à la base de données
-conn = creer_connexion(host, user, password, database)
+conn = creer_connexion(host, port, user, password, database)
 
 # Assurez-vous de fermer la connexion une fois que vous avez fini
-if conn is not None and conn.is_connected():
+if conn is not None:
     conn.close()
