6d7b140dadf655858291509dd3d82878a5d9d14f
Ivan Mustiere <ivanmustiere.pro@gmail.com>
2023-11-14 12:24:27 +0100

    Update

--- Script/DiffVersion.py
+++ Script/DiffVersion.py
@@ -1,50 +1,43 @@
 from deepdiff import DeepDiff
 import json
-
-MAX_LINE_LENGTH = 150
-
-def tronquer_ligne(ligne):
-    return (ligne[:MAX_LINE_LENGTH] + '...') if len(ligne) > MAX_LINE_LENGTH else ligne
+import os
 
 def charger_json(fichier):
     with open(fichier, 'r') as f:
         return json.load(f)
 
 def afficher_differences(diff):
+    # Retourne True s'il y a des changements significatifs
     if not diff:
-        print("Aucune différence trouvée.")
-        return
+        return False
 
     for type_diff, details in diff.items():
-        # Vérifie si la seule différence est la version
         if type_diff == 'values_changed' and len(details) == 1 and "root['version']" in details:
-            print("Aucun changement significatif à part la mise à jour de la version.")
-            continue
-        print(f"{type_diff}:")
-        for chemin, val in details.items():
-            if not chemin == "root['version']":
-                if type_diff == 'values_changed':
-                    old_value = val['old_value']
-                    new_value = val['new_value']
-                    ligne = f" - {chemin} : de '{old_value}' à '{new_value}'"
-                    print(tronquer_ligne(ligne))
-                else:
-                    ligne = f" - {chemin} : {val}"
-                    print(tronquer_ligne(ligne))
+            return False
+    return True
+
+def comparer_versions(dossier1, dossier2):
+    # Lister tous les fichiers dans les deux dossiers
+    fichiers1 = {f for f in os.listdir(dossier1) if f.endswith('.json')}
+    fichiers2 = {f for f in os.listdir(dossier2) if f.endswith('.json')}
+
+    # Comparer seulement les fichiers qui existent dans les deux dossiers
+    fichiers_communs = fichiers1.intersection(fichiers2)
+
+    for fichier in fichiers_communs:
+        chemin1 = os.path.join(dossier1, fichier)
+        chemin2 = os.path.join(dossier2, fichier)
+
+        json1 = charger_json(chemin1)
+        json2 = charger_json(chemin2)
+
+        differences = DeepDiff(json1, json2, ignore_order=True)
+
+        if afficher_differences(differences):
+            print(f"Des changements significatifs ont été réalisés dans {fichier}")
 
 # Exemple d'utilisation
-fichier1 = 'Json/13.21.1/Graves.json'
-fichier2 = 'Json/13.22.1/Graves.json'
+dossier1 = 'Json/13.21.1'
+dossier2 = 'Json/13.22.1'
 
-json1 = charger_json(fichier1)
-json2 = charger_json(fichier2)
-
-differences = DeepDiff(json1, json2, ignore_order=True)
-print()
-afficher_differences(differences)
-print()
-
-
-
-
-
+comparer_versions(dossier1, dossier2)
